1712e71304ea615d936fe3a153596a3124e95774
adventl <aadventt@gmail.com>
2025-12-02 00:29:58 +0800

    Add OrbitControls, and ColorPicker for box

--- src/canvas/canvas-container.tsx
+++ src/canvas/canvas-container.tsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Canvas } from '@react-three/fiber'
 import { ColorPicker, Flex } from 'antd';
+import { OrbitControls } from '@react-three/drei';
+import type { ColorPickerProps, GetProp } from 'antd';
+
+type Color = Extract<GetProp<ColorPickerProps, 'value'>, string | { cleared: any }>;
 
 export const CanvasContainer: React.FC = React.memo(() => {
-    return (
-    <Flex>
-      <ColorPicker defaultValue="#1677ff" open={true} />
+  const [color, setColor] = useState<Color>('#ffffff');
+
+  const hexString = React.useMemo<string>(
+    () => (typeof color === 'string' ? color : color?.toHexString()),
+    [color],
+  );
+
+  return (
+    <Flex justify='center' vertical className='h-full w-full bg-red-200'>
+      <ColorPicker defaultValue={color} open={true} onChange={setColor} />
       <Canvas>
-        <mesh onClick={() => alert('Hellooo')}>
-          <boxGeometry args={[2, 4, 2]} />
-          <meshPhongMaterial />
+        <OrbitControls minPolarAngle={0} maxPolarAngle={Math.PI / 2.1} />
+        <mesh>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshBasicMaterial color={hexString} />
         </mesh>
-        <ambientLight intensity={0.1} />
-        <directionalLight position={[0, 0, 5]} color="red" />
+        <mesh position={[2, 0, 0]}>
+          <sphereGeometry args={[1, 1000, 1000]} />
+          <meshNormalMaterial />
+        </mesh>
+        <ambientLight intensity={1} />
+        <spotLight position={[0, 0, 0]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} color={color.toString()} />
       </Canvas>
     </Flex>
 
